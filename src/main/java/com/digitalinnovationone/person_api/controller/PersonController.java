8edca62923b40ef2432dfc240b695d239feb416e
MONIKERAJAVA <monica.dominog@gmail.com>
2021-07-06 19:09:05 -0700

    Subida inicial do projeto

---
+++ src/main/java/com/digitalinnovationone/person_api/controller/PersonController.java
@@ -0,0 +1,15 @@
+package com.digitalinnovationone.person_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+    @GetMapping
+    public String getBook() {
+        return "API REST no ar !";
+
+    }
+}
---
+++ src/test/java/com/digitalinnovationone/person_api/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.person_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
